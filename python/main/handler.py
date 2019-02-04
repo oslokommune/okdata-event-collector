ed9908e866d8b9f188d66e6e72716e6e72e6ef53
oyvind.nygard@knowit.no <oyvind.nygard@knowit.no>
2019-02-04 16:34:28 +0100

    added unittests for various util methods

--- python/main/handler.py
+++ python/main/handler.py
@@ -2,35 +2,47 @@ import json
 import boto3
 
 
-def post_event(event, context, retries = 3):
+def post_event(event, context, retries=3):
     dataset_id, dataset_version = event['pathParameters']['datasetId'], event['pathParameters']['version']
     if not get_metadata(dataset_id, dataset_version):
         return not_found_response(dataset_id, dataset_version)
 
     record_data_list = event_to_record_data_list(event)
-    stream_name=f'incoming.{dataset_id}.{dataset_version}'
-    put_records_response = put_records_to_kinesis(record_data_list, stream_name)
+    stream_name = f'incoming.{dataset_id}.{dataset_version}'
+    put_records_response, failed_record_list = put_records_to_kinesis(record_data_list, stream_name, retries)
+
+    if put_records_response['FailedRecordCount'] > 0:
+        return failed_records_response(put_records_response, )
+
+    return ok_response()
+
+
+def put_records_to_kinesis(record_data_list, stream_name, retries):
+    kinesis_client = boto3.client('kinesis', region_name='eu-west-1')
+    put_records_response = kinesis_client.put_records(
+        StreamName=stream_name,
+        Records=record_data_list
+    )
 
     # Applying retry-strategy: https://docs.aws.amazon.com/streams/latest/dev/developing-producers-with-sdk.html
     while put_records_response['FailedRecordCount'] > 0 & retries > 0:
-        failed_record_list = []
-        for i in range(record_data_list.size()):
-            if 'ErrorCode' in put_records_response['Records']:
-                failed_record_list.append(record_data_list[i])
-        record_data_list = failed_record_list
-        put_records_response = put_records_to_kinesis(record_data_list, stream_name)
+
+        record_data_list = get_failed_records(put_records_response, record_data_list)
+
+        put_records_response = kinesis_client.put_records(
+            StreamName=stream_name,
+            Records=record_data_list
+        )
         retries -= 1
+    return put_records_response, get_failed_records(put_records_response, record_data_list)
 
-    return accepted_response(put_records_response['FailedRecordCount'])
 
-
-def put_records_to_kinesis(record_data_list, stream_name):
-    kinesis_client = boto3.client('kinesis', region_name='eu-west-1')
-    response = kinesis_client.put_records(
-        StreamName=stream_name,
-        Records = record_data_list
-    )
-    return response
+def get_failed_records(put_records_response, record_data_list):
+    failed_record_list = []
+    for i in range(len(record_data_list)):
+        if 'ErrorCode' in put_records_response['Records'][i]:
+            failed_record_list.append(record_data_list[i])
+    return failed_record_list
 
 
 def get_metadata(dataset_id, dataset_version):
@@ -43,7 +55,8 @@ def event_to_record_data_list(event):
     record_data_list = []
 
     for element in json.loads(event['body']):
-        record_data = {'data': element,'datasetId': event['pathParameters']['datasetId'],'version': event['pathParameters']['version']}
+        record_data = {'data': element, 'datasetId': event['pathParameters']['datasetId'],
+                       'version': event['pathParameters']['version']}
         record_data_list.append(
             {
                 'Data': json.dumps(record_data),
@@ -54,10 +67,21 @@ def event_to_record_data_list(event):
     return record_data_list
 
 
-def accepted_response(failed_record_count):
+def failed_records_response(failed_record_list):
     lambda_proxy_response = {
-        'statusCode': 202,
-        'body': json.dumps({'failedRecordCount': failed_record_count})
+        'statusCode': 500,
+        'body': json.dumps({
+            'message': 'Request failed for some elements',
+            'failedElements': failed_record_list
+        })
+    }
+    return lambda_proxy_response
+
+
+def ok_response():
+    lambda_proxy_response = {
+        'statusCode': 200,
+        'body': json.dumps({'message': 'Ok'})
     }
     return lambda_proxy_response
 
--- python/tests/handler_test.py
+++ python/tests/handler_test.py
@@ -4,20 +4,36 @@ import json
 
 import python.main.handler as handler
 import python.tests.test_data.event_to_record_data as event_to_record_data
+import python.tests.test_data.get_failed_records_data as get_failed_records_data
 from moto import mock_kinesis
 
 
 class Tester(unittest.TestCase):
-
     def test_event_to_record_data_list(self):
         event = event_to_record_data.input_event
         record_data_list = handler.event_to_record_data_list(event)
-        print(record_data_list)
         self.assertListEqual(record_data_list, event_to_record_data.expected)
 
-    # @mock_kinesis
-    # def test_post_record(self):
-    #     assert True
+    def test_get_failed_records(self):
+        failed_records_list = handler.get_failed_records(
+            get_failed_records_data.put_records_response, get_failed_records_data.record_data_list)
+        self.assertListEqual(failed_records_list, get_failed_records_data.expected)
+
+    @mock_kinesis
+    def test_put_records_to_kinesis(self):
+        conn = boto3.client('kinesis', region_name='eu-west-1')
+        stream_name = 'test_stream'
+        conn.create_stream(StreamName=stream_name, ShardCount=1)
+        record_list = []
+        for i in range(100):
+            record_list.append({
+                'PartitionKey': 'aa-bb',
+                'Data': '{"data": {"key30": "value30", "key31": "value31"}, "datasetId": "d123", "version": "v123"}'
+            })
+        put_records_response = handler.put_records_to_kinesis(record_list, stream_name, 3)
+
+        assert put_records_response[0]['FailedRecordCount'] == 0
+        assert put_records_response[1] == []
 
 
 if __name__ == '__main__':
