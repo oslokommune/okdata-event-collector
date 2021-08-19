07eadc428bcb27fb0351bca798bf28c4f952e632
Fredrik Vraalsen <fredrik@vraalsen.no>
2021-08-19 09:54:09 +0200

    DP-2197 Upgrade resource-auth library to fix Keycloak retries

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
         "jsonschema",
         "python-keycloak",
         "okdata-aws",
-        "okdata-resource-auth>=0.1.2",
+        "okdata-resource-auth>=0.1.3",
         "okdata-sdk >= 0.9.0",
     ],
 )
